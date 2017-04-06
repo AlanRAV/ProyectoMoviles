0c929bceeb73a4ec5cc394d063012b8fa3b51905
juanheuforico95 <juanheuforcio@gmail.com>
2017-04-06 18:58:54 -0500

    incorporar caldroid y listciew

--- app/src/main/java/com/example/alan_/proyectomoviles/AddGroup.java
+++ app/src/main/java/com/example/alan_/proyectomoviles/AddGroup.java
@@ -1,13 +1,28 @@
 package com.example.alan_.proyectomoviles;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AddGroup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_group);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("");
+
+
+        //instantiate custom adapter
+        MyCustomAdapter adapter = new MyCustomAdapter(list, this);
+
+        //handle listview and assign adapter
+        ListView lView = (ListView)findViewById(R.id.leads_list);
+        lView.setAdapter(adapter);
+
     }
 }
